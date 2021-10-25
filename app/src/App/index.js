b0762089301d9b5ae28c7a0f7b6703db7f25d6de
ZelmaSedano <zvsedano@gmail.com>
2021-10-25 15:48:18 -0500

    extracting API component

--- app/src/App/index.js
+++ app/src/App/index.js
@@ -10,6 +10,8 @@ import useApi from "../auth/useApi";
 import useAuth0 from "../auth/useAuth0";
 import { Protected } from "../auth/widgets";
 
+import API from "./API";
+
 import "./App.css";
 // test comment
 
@@ -89,90 +91,6 @@ const Dashboard = (addTask) => {
   );
 };
 
-const API = () => {
-  const { apiClient } = useApi();
-
-  const [responseObj, setResponseObj] = React.useState({});
-  const [quote, setQuote] = React.useState("");
-  const [author, setAuthor] = React.useState("");
-
-  // add new Affirmation/task
-  const [tasks, setTasks] = React.useState([]);
-  const [task, setTask] = React.useState("");
-  const loadTasks = React.useCallback(
-    async () => setTasks(await apiClient.getTasks()),
-    [apiClient],
-  );
-  const addTask = (taskName) => apiClient.addTask(taskName).then(loadTasks);
-
-  const canAdd = task !== "";
-
-  const handleChange = () => {
-    setTask(quote);
-    addTask(task);
-  };
-
-  // fetch should be inside a useEffect so it doesn't get caught in an infinite loop when assigning response to 'setResponseObj' state
-  useEffect(() => {
-    fetch(
-      `https://quotes.rest/qod?language=en`,
-      // `https://api.openweathermap.org/data/2.5/weather?units=${unit}&q=${uriEncodedCity}&appid=81eaae9c9ea6f28f239fe73eebafd259`
-      // {
-      //   method: 'GET',
-      //   headers: {
-      //     'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-      //     'x-rapidapi-key': `81eaae9c9ea6f28f239fe73eebafd259`,
-      //   },
-      // }
-    )
-      .then((response) => response.json())
-
-      .then((response) => {
-        console.log(response);
-        setResponseObj(response);
-        setQuote(response.contents.quotes[0].quote);
-        setAuthor(response.contents.quotes[0].author);
-        // console.log("response.status is " + response.status);
-        // 2nd version: 200 is a string
-        // // in this API, we had to use success to check if it passed or not
-        if (!response.success) {
-          throw new Error();
-        }
-
-        // console.log("this is the responseObj");
-        // takes take the json & saves json response as responseObj state
-        // setLoading(false);
-      });
-  }, []);
-  console.log("quotes are", quote);
-
-  // I want to uncomment this
-  // .catch((err) => {
-  //   setError(true);
-  //   // setLoading(false);
-  //   console.log("error");
-  // });
-
-  return (
-    <div className="quote-wrapper">
-      <h2 className="quote-header">Quote of the Day</h2>
-      <form>
-        <p className="quote-p">
-          "{quote}" - {author}
-        </p>
-
-        {/* <button onClick={handleChange} className="add-btn-1" disabled={!canAdd}>
-          Add
-        </button> */}
-        <p className="add-p">
-          * If you like this quote, feel free to copy & paste into Affirmations
-          Field above
-        </p>
-      </form>
-    </div>
-  );
-};
-
 const About = () => {
   return (
     <div className="about-wrapper">
@@ -213,7 +131,7 @@ const AboutText = () => {
         <br />
         <p className="about-p">
           Science says it works, so try it today! And don't forget, if you want
-          to receive your favorite mention as a reinder via email, just click
+          to receive your favorite mention as a reminder via email, just click
           the checkbox next to that mention!
         </p>
       </div>
